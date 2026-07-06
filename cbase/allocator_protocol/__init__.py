fa2d320c2a2cfce0a0597fc5748808f7231f5044
Han Bolun <Bolun.Han@outlook.com>
2026-07-06 14:51:27 +0800

    Add __init__.py to expose allocator interfaces and implementations

---
+++ cbase/allocator_protocol/__init__.py
@@ -0,0 +1,28 @@
+from .c_heap_allocator import (
+    ALLOCATOR as HEAP_ALLOCATOR,
+    HeapAllocator,
+    HeapMemoryBlock,
+    HeapMemoryPage
+)
+
+from .c_shm_allocator import (
+    ALLOCATOR as SHM_ALLOCATOR,
+    SharedMemoryAllocator,
+    SharedMemoryBlock,
+    SharedMemoryPage,
+    cleanup as shm_cleanup
+)
+
+from .c_allocator_protocol import (
+    AllocatorProtocol,
+    AP_FREELIST,
+    AP_LOCKED,
+    AP_SHARED,
+    EnvConfigContext
+)
+
+__all__ = [
+    'HEAP_ALLOCATOR', 'HeapAllocator', 'HeapMemoryBlock', 'HeapMemoryPage',
+    'SHM_ALLOCATOR', 'SharedMemoryAllocator', 'SharedMemoryBlock', 'SharedMemoryPage', 'shm_cleanup',
+    'AllocatorProtocol', 'AP_FREELIST', 'AP_LOCKED', 'AP_SHARED', 'EnvConfigContext'
+]
