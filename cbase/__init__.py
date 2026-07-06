d2e05e3aa97d6adfdae673862e992616844d69ca
Han Bolun <Bolun.Han@outlook.com>
2026-07-06 14:51:32 +0800

    Add __init__.py to define package version and include directories

---
+++ cbase/__init__.py
@@ -0,0 +1,26 @@
+__version__ = '0.10.7'
+
+import functools
+import os
+import pathlib
+
+
+@functools.cache
+def get_include() -> list[str]:
+    res_dir = pathlib.Path(__file__).parent
+
+    src_dirs = [
+        os.path.realpath(res_dir),
+        os.path.realpath(res_dir / 'allocator_protocol'),
+    ]
+
+    include_root = os.path.realpath(res_dir / 'includes')
+    if os.path.isdir(include_root):
+        src_dirs.append(include_root)
+
+    return src_dirs
+
+
+__all__ = [
+    'get_include',
+]
