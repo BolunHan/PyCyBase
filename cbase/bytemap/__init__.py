a17e7b789aae83764a35e2396b04bc9b007b8f0c
Han Bolun <Bolun.Han@outlook.com>
2026-07-21 18:13:23 +0800

    feat: add __init__.py for bytemap and intern_string modules

---
+++ cbase/bytemap/__init__.py
@@ -0,0 +1,21 @@
+from .c_bytemap import (
+    ByteMapEx,
+    ByteMapExDouble,
+    ByteMap,
+    BoundByteMapEx,
+    BoundByteMapExDouble,
+    BoundByteMap,
+    BoundByteSet,
+    ByteMapPerformanceTestToolkit,
+)
+
+__all__ = [
+    'ByteMapEx',
+    'ByteMapExDouble',
+    'ByteMap',
+    'BoundByteMapEx',
+    'BoundByteMapExDouble',
+    'BoundByteMap',
+    'BoundByteSet',
+    'ByteMapPerformanceTestToolkit',
+]
---
+++ cbase/intern_string/__init__.py
@@ -0,0 +1,19 @@
+from .c_intern_string import (
+    InternString,
+    InternStringPool,
+    IstrTestToolkit,
+    POOL,
+    INTRA_POOL,
+    C_POOL,
+    C_INTRA_POOL,
+)
+
+__all__ = [
+    'InternString',
+    'InternStringPool',
+    'IstrTestToolkit',
+    'POOL',
+    'INTRA_POOL',
+    'C_POOL',
+    'C_INTRA_POOL',
+]
