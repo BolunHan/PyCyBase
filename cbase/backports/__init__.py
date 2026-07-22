30c0074a6c1dc42cf6e00e8980bda206b48adb23
Han Bolun <Bolun.Han@outlook.com>
2026-07-22 13:02:43 +0800

    feat: add backports for 128-bit integer support in __init__.py

---
+++ cbase/backports/__init__.py
@@ -0,0 +1,9 @@
+from cbase.backports.pylong import (
+    pylong_from_uint128,
+    pylong_from_int128,
+    pylong_as_uint128,
+    pylong_as_int128,
+    _UINT128_MAX,
+    _INT128_MAX,
+    _INT128_MIN
+)
